852af2a3134d5499a7525ddb763f3da062bb1b69
Hyeonjun Park <hyeonjunpark@Hyeonjuns-iMac.local>
2022-05-02 17:54:06 +0900

    practice 1.24

--- ch1/practice_1.23.scm
+++ ch1/practice_1.23.scm
@@ -2,25 +2,16 @@
 
 ;; 1.23
 
-;;  *** 100000000019
-;;  *** 100000000057
-;; .7399999999999949
-
-;; (search-for-primes 1000000000000)
-;;  *** 1000000000039
-;;  *** 1000000000061
-;;  *** 1000000000063
-;; 2.2199999999999998 (* 3.0000000000000204)
-
-;; 29 (* 3.3256880733944953)
-
 ;; version 1
+(define (ff-prime? n) (fast-prime? n 100))
 
 (define (time-test f n count start-time)
   (cond ((= count 0)
           (display (- (runtime) start-time))
           (newline))
-        (else (time-test f n (- count 1) start-time))))
+        (else
+         (f n)
+         (time-test f n (- count 1) start-time))))
       
   
 
@@ -30,37 +21,36 @@
 (define (find-divisor-1 n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor-1 n (+ test-divisor 1)))))
 
-;; (time-test smallest-divisor-1 100000000003 1000000 (runtime))
-;; .3199999999999932
-;; (time-test smallest-divisor-1 100000000019 1000000 (runtime))
-;; .3200000000000074
+
+;; (time-test smallest-divisor-1 100000000003 10 (runtime))
+;; 2.329999999999984
+;; (time-test smallest-divisor-1 100000000019 10 (runtime))
+;; 2.330000000000041
 ;; (time-test smallest-divisor-1 100000000057 1000000 (runtime))
-;; .30999999999998806
+;; 2.339999999999975
 
-;; (time-test smallest-divisor-1 1000000000039 1000000 (runtime))
-;; .3100000000000023
+;; (time-test smallest-divisor-1 1000000000039 10 (runtime))
+;; 7.439999999999998
+;; (time-test smallest-divisor-1 1000000000061 10 (runtime))
+;; 7.3799999999999955
+;; (time-test smallest-divisor-1 1000000000063 10 (runtime))
+;; 7.360000000000014
 
-;; (time-test smallest-divisor-1 1000000000061 1000000 (runtime))
-;; .3100000000000023
+;; (time-test smallest-divisor-1 10000000000037 10 (runtime))
+;; 23.409999999999968
+;; (time-test smallest-divisor-1 10000000000051 10 (runtime))
+;; 23.329999999999984
+;; (time-test smallest-divisor-1 10000000000099 10 (runtime))
+;; 23.399999999999977
 
-;; (time-test smallest-divisor-1 1000000000063 1000000 (runtime))
-;; .3199999999999932
-
-;; (time-test smallest-divisor-1 10000000000037 1000000 (runtime))
-;; .3100000000000023
-;; (time-test smallest-divisor-1 10000000000051 1000000 (runtime))
-;; .3299999999999983
-;; (time-test smallest-divisor-1 10000000000099 1000000 (runtime))
-;; .3200000000000074
-
-;; (time-test smallest-divisor-1 100000000000031 1000000 (runtime))
-;; .30999999999998806
-;; (time-test smallest-divisor-1 100000000000067 1000000 (runtime))
-;; .3100000000000023
-;; (time-test smallest-divisor-1 100000000000097 1000000 (runtime))
-;; .3200000000000074
+;; (time-test smallest-divisor-1 100000000000031 10 (runtime))
+;; 73.57999999999998
+;; (time-test smallest-divisor-1 100000000000067 10 (runtime))
+;; 73.78000000000009
+;; (time-test smallest-divisor-1 100000000000097 10 (runtime))
+;; 73.85000000000002
 
 ;; version 2
 
@@ -70,17 +60,40 @@
 (define (find-divisor-2 n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor-2 n (next test-divisor)))))
 
 (define (next x)
-  (if (even? x)
-      (+ x 1)
+  (if (= x 2)
+      3
       (+ x 2)))
 
 (define (divides? a b)
   (= (remainder b a) 0))
 
-;; (time-test smallest-divisor-2 100000000000067 1000000 (runtime))
-;; .3100000000000023
-;; (time-test smallest-divisor-2 100000000000097 1000000 (runtime))
-;; .3200000000000074
+;; (time-test smallest-divisor-2 100000000003 10 (runtime))
+;; 1.490000000000009   ;; 1.5637583892617248 times
+;; (time-test smallest-divisor-1 100000000019 10 (runtime))
+;; 1.4399999999999977  ;; 1.6180555555555864 times
+;; (time-test smallest-divisor-1 100000000057 1000000 (runtime))
+;; 1.4399999999999977  ;; 1.6249999999999851 times
+
+;; (time-test smallest-divisor-2 1000000000039 10 (runtime))
+;; 4.600000000000023  ;; 1.6173913043478176 times
+;; (time-test smallest-divisor-2 1000000000061 10 (runtime))
+;; 4.589999999999975  ;; 1.6078431372549098 times
+;; (time-test smallest-divisor-2 1000000000063 10 (runtime))
+;; 4.589999999999975  ;; 1.6034858387799682 times
+
+;; (time-test smallest-divisor-2 10000000000037 10 (runtime))
+;; 14.32000000000005  ;; 1.6347765363128413
+;; (time-test smallest-divisor-2 10000000000051 10 (runtime))
+;; 14.410000000000025  ;; 1.6190145732130425 times
+;; (time-test smallest-divisor-2 10000000000099 10 (runtime))
+;; 14.480000000000018   ;; 1.6160220994475103 times
+
+;; (time-test smallest-divisor-2 100000000000031 10 (runtime))
+;; 45.329999999999984 ;; 1.6232075887932937 times
+;; (time-test smallest-divisor-2 100000000000067 10 (runtime))
+;; 45.239999999999895 ;; 1.630857648099033 times
+;; (time-test smallest-divisor-2 100000000000097 10 (runtime))
+;; 45.30000000000007 ;; 1.630242825607062 times
